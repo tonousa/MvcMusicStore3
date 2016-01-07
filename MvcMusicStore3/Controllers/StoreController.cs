1e6e03be692e73a57299661c99be790e8f941aa0
tonousa <xgood12>
2016-01-07 13:48:37 -0500

    Store Controller 1

---
+++ MvcMusicStore3/Controllers/StoreController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore3.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public String Index()
+        {
+            return "Hello from the store.Index()";
+        }
+
+        public String Browse()
+        {
+            return "Hello from the Store.Browse()";
+        }
+
+        public String Details()
+        {
+            return "Hello from the Store.Details()";
+        }
+    }
+}
