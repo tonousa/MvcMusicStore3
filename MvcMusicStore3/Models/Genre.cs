ba9c72cac3c1768d4c5c5b00dda506f3322b3ddc
tonousa <tonousa@hotmail.com>
2016-01-16 10:18:09 -0500

    models

---
+++ MvcMusicStore3/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore3.Models
+{
+    public class Genre
+    {
+        public string Name { get; set; }
+    }
+
+
+}
