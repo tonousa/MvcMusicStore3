220b34f535c77d1d140303fab55d560bedf27c46
tonousa <tonousa@hotmail.com>
2016-01-19 14:02:10 -0500

    Before Authentication

--- MvcMusicStore3/App_Start/BundleConfig.cs
+++ MvcMusicStore3/App_Start/BundleConfig.cs
@@ -10,7 +10,8 @@ namespace MvcMusicStore3
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include
                 ("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include
-                ("~/Scripts/jquery.validate.js"));
+                ("~/Scripts/jquery.validate*", 
+                "~/Scripts/jquery.unobtrusive*"));
         }
     }
 }
