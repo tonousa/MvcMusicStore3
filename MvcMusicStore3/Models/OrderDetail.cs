45dda84f2aa7c5aeea68e58e7165cee883a0e4ea
tonousa <tonousa@hotmail.com>
2016-01-21 08:35:03 -0500

    adding classes

--- MvcMusicStore3/Models/OrderDetail.cs
+++ MvcMusicStore3/Models/OrderDetail.cs
@@ -10,7 +10,6 @@ namespace MvcMusicStore3.Models
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public virtual Album Album { get; set; }
-        public virtual Album Album { get; set; }
         public virtual Order Order { get; set; }
     }
 }
