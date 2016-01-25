39ccb55c120d44842487ff0c57a8eeeeb31233ee
tonousa <tonousa@hotmail.com>
2016-01-24 19:32:25 -0500

    Test checkout and finish

--- MvcMusicStore3/Controllers/HomeController.cs
+++ MvcMusicStore3/Controllers/HomeController.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore3.Models;
 
 namespace MvcMusicStore3.Controllers
 {
     public class HomeController : Controller
     {
+        public MusicStoreEntities storeDB = new MusicStoreEntities();
+
         //
         // GET: /Home/
-
         public ActionResult Index()
         {
             //return "Hello from Home";
-            return View();
+            // Get the most popular albums
+            var albums = GetTopSellingAlbums(5);
+            return View(albums);
         }
 
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+
+
+       
     }
 }
